8eacd20836c5c00f36b6e8082b47810198b0b104
Ondřej Surý <ondrej@sury.org>
2019-06-18 17:29:50 +0200

    Improve building the manpages

--- doc/arm/conf.py
+++ doc/arm/conf.py
@@ -47,7 +47,20 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build',
+                    'Thumbs.db',
+                    '.DS_Store',
+                    '*.grammar.rst',
+                    '*.zoneopts.rst',
+                    'catz.rst',
+                    'dlz.rst',
+                    'dnssec.rst',
+                    'dyndb.rst',
+                    'logging-cattegories.rst',
+                    'managed-keys.rst',
+                    'pkcs11.rst',
+                    'plugins.rst'
+                    ]
 
 # The master toctree document.
 master_doc = 'index'
