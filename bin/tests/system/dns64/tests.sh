ad7d85d118cc06e75261e52acb22a5d263ec37af
Mark Andrews <marka@isc.org>
2019-08-06 17:28:02 +1000

    Test use-dns64 by simulation connecting to a IPv4 only server through a NAT64.

--- bin/tests/system/dns64/tests.sh
+++ bin/tests/system/dns64/tests.sh
@@ -1400,5 +1400,14 @@ n=`expr $n + 1`
 if [ $ret != 0 ]; then echo_i "failed"; fi
 status=`expr $status + $ret`
 
+echo_i "checking use-dns64 ($n)"
+ret=0
+$DIG $DIGOPTS @10.53.0.3 no-aaaa aaaa > dig.out.ns3.test$n || ret=1
+grep "status: NOERROR" dig.out.ns3.test$n > /dev/null || ret=1
+grep "fd92:7065:b8e:fffe::102:304" dig.out.ns3.test$n > /dev/null || ret=1
+n=`expr $n + 1`
+if [ $ret != 0 ]; then echo_i "failed"; fi
+status=`expr $status + $ret`
+
 echo_i "exit status: $status"
 [ $status -eq 0 ] || exit 1
